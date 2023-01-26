fdffa8af610c420bf65acc101c2b8a1001d58f2f
Matiko-Codes <mmatiko243@gmail.com>
2023-01-26 15:52:35 +0300

    2 blank lines found 1

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Module to return pascal triangle'''
 
+
 def pascal_triangle(n):
     """
     Function that returns a list of lists of integers representing the Pascal's triangle of n.
