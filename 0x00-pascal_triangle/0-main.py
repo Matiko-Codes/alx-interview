694ead65abba3cab7c01dcc1cd09cbf4ace90394
Matiko-Codes <mmatiko243@gmail.com>
2023-01-26 11:38:52 +0300

    Module

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 '''Module to return pascal triangle'''
 
-#!/usr/bin/python3
-"""
-0-main
-"""
-pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
-
-def print_triangle(triangle):
+def pascal_triangle(n):
     """
-    Print the triangle
+    Function that returns a list of lists of integers representing the Pascal's triangle of n.
+    If n <= 0, an empty list is returned.
     """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
+    if n <= 0:
+        return []
+    # create empty 2D list with n rows and i+1 columns for the first i rows
+    triangle = [[1] * (i + 1) for i in range(n)]
+    
+    # loop through the rows
+    for i in range(n):
+        # loop through the columns of each row
+        for j in range(1, i):
+            # fill in value of each element using the formula for Pascal's triangle
+            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+    return triangle
