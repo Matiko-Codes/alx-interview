e650696f0e5c8d463547549b1e23eb3c8def7d58
Matiko-Codes <mmatiko243@gmail.com>
2023-02-18 09:00:31 +0300

    Change

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,46 +4,35 @@ module contains a script that reads stdin line by line and computes metrics
 """
 import sys
 
+status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+status_dict = {code: 0 for code in status_codes}
+total_size = 0
+line_count = 0
 
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+try:
+    for line in sys.stdin:
+        line_count += 1
+        split_line = line.split()
+        if len(split_line) < 7:
+            continue
+        if not split_line[6].isdigit():
+            continue
+        if split_line[5][1:] != "/projects/260":
+            continue
+        status_code = split_line[8]
+        if status_code in status_codes:
+            status_dict[status_code] += 1
+        total_size += int(split_line[6])
+        if line_count % 10 == 0:
+            print("File size: {}".format(total_size))
+            for code in sorted(status_dict.keys()):
+                if status_dict[code] > 0:
+                    print("{}: {}".format(code, status_dict[code]))
+            print("")
 
-
-file_size = 0
-
-
-def print_metrics():
-    """prints of the logs"""
-    print("File size: {}".format(file_size))
-    for status in sorted(status_codes.keys()):
-        if status_codes[status]:
-            print("{}: {}".format(status, status_codes[status]))
-
-
-if __name__ == "__main__":
-    count = 0
-    try:
-        for line in sys.stdin:
-            try:
-                elems = line.split()
-                file_size += int(elems[-1])
-                if elems[-2] in status_codes:
-                    status_codes[elems[-2]] += 1
-            except Exception:
-                pass
-            if count == 9:
-                print_metrics()
-                count = -1
-            count += 1
-    except KeyboardInterrupt:
-        print_metrics()
-        raise
-    print_metrics()
+except KeyboardInterrupt:
+    print("File size: {}".format(total_size))
+    for code in sorted(status_dict.keys()):
+        if status_dict[code] > 0:
+            print("{}: {}".format(code, status_dict[code]))
+    print("")
