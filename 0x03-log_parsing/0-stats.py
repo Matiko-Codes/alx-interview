98eef00497bbf573e0a63a3c0cdcd1983da70011
Matiko-Codes <mmatiko243@gmail.com>
2023-02-18 08:43:45 +0300

    Change

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,61 +1,48 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 module contains a script that reads stdin line by line and computes metrics
 """
-
 import sys
 
-# Initialize variables
-total_size = 0
+
 status_codes = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0,
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
 }
-line_count = 0
 
-# Loop through stdin line by line
-for line in sys.stdin:
-    # Parse line and extract file size and status code
-    fields = line.strip().split(' ')
-    if len(fields) != 7:
-        continue
+file_size = 0
+
+
+def print_metrics():
+    """prints of the logs"""
+    print("File size: {}".format(file_size))
+    for status in sorted(status_codes.keys()):
+        if status_codes[status]:
+            print("{}: {}".format(status, status_codes[status]))
+
+
+if __name__ == "__main__":
+    count = 0
     try:
-        file_size = int(fields[6])
-        status_code = int(fields[5])
-    except ValueError:
-        continue
-    
-    # Update variables
-    total_size += file_size
-    status_codes[status_code] += 1
-    line_count += 1
-    
-    # Print metrics every 10 lines
-    if line_count % 10 == 0:
-        print(f'Total file size: {total_size}')
-        for code in sorted(status_codes.keys()):
-            if status_codes[code] > 0:
-                print(f'{code}: {status_codes[code]}')
-        print()
-    
-    # Handle keyboard interrupt (CTRL+C)
-    try:
-        pass
+        for line in sys.stdin:
+            try:
+                elems = line.split()
+                file_size += int(elems[-1])
+                if elems[-2] in status_codes:
+                    status_codes[elems[-2]] += 1
+            except Exception:
+                pass
+            if count == 9:
+                print_metrics()
+                count = -1
+            count += 1
     except KeyboardInterrupt:
-        print(f'Total file size: {total_size}')
-        for code in sorted(status_codes.keys()):
-            if status_codes[code] > 0:
-                print(f'{code}: {status_codes[code]}')
-        sys.exit(0)
-
-# Print final metrics
-print(f'Total file size: {total_size}')
-for code in sorted(status_codes.keys()):
-    if status_codes[code] > 0:
-        print(f'{code}: {status_codes[code]}')
+        print_metrics()
+        raise
+    print_metrics()
